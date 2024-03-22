33c75130acb6d4ae94bfb34d16785601b5ce91b7
TjanKazar <122014731+TjanKazar@users.noreply.github.com>
2024-03-22 22:15:31 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace VIZ_N3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            save_Derypt_file = new Button();
+            browse = new Button();
+            saveFileDialog1 = new SaveFileDialog();
+            comboBox1 = new ComboBox();
+            save_iv = new Button();
+            save_encrypt_file = new Button();
+            SuspendLayout();
+            // 
+            // save_Derypt_file
+            // 
+            save_Derypt_file.Location = new Point(570, 39);
+            save_Derypt_file.Name = "save_Derypt_file";
+            save_Derypt_file.Size = new Size(112, 45);
+            save_Derypt_file.TabIndex = 1;
+            save_Derypt_file.Text = "Save Decrypted File";
+            save_Derypt_file.UseVisualStyleBackColor = true;
+            save_Derypt_file.Click += save_file_Click;
+            // 
+            // browse
+            // 
+            browse.Location = new Point(400, 44);
+            browse.Name = "browse";
+            browse.Size = new Size(75, 34);
+            browse.TabIndex = 2;
+            browse.Text = "Browse";
+            browse.UseVisualStyleBackColor = true;
+            browse.Click += browse_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(899, 137);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 3;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // save_iv
+            // 
+            save_iv.Location = new Point(945, 44);
+            save_iv.Name = "save_iv";
+            save_iv.Size = new Size(75, 34);
+            save_iv.TabIndex = 4;
+            save_iv.Text = "Save IV";
+            save_iv.UseVisualStyleBackColor = true;
+            save_iv.Click += save_iv_Click;
+            // 
+            // save_encrypt_file
+            // 
+            save_encrypt_file.Location = new Point(733, 39);
+            save_encrypt_file.Name = "save_encrypt_file";
+            save_encrypt_file.Size = new Size(138, 45);
+            save_encrypt_file.TabIndex = 5;
+            save_encrypt_file.Text = "Save Encrypted File As Text File";
+            save_encrypt_file.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1164, 634);
+            Controls.Add(save_encrypt_file);
+            Controls.Add(save_iv);
+            Controls.Add(comboBox1);
+            Controls.Add(browse);
+            Controls.Add(save_Derypt_file);
+            Name = "Form1";
+            Text = "FIle Enctyption";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Button save_Derypt_file;
+        private Button browse;
+        private SaveFileDialog saveFileDialog1;
+        private ComboBox comboBox1;
+        private Button save_iv;
+        private Button save_encrypt_file;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace VIZ_N3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add(128);
+            comboBox1.Items.Add(192);
+            comboBox1.Items.Add(256);
+            comboBox1.Items.Add(1024);
+            comboBox1.Items.Add(2048);
+        }
+        public int keysize;
+        public byte[] fileBytes;
+        public string fileType;
+        byte[] iv = Encoding.UTF8.GetBytes("Hello wrld");
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            keysize = (int)comboBox1.SelectedItem;
+        }
+
+        private void browse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Multiselect = false;
+            DialogResult userClickedOK = openFileDialog1.ShowDialog();
+
+            if (userClickedOK == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                fileBytes = File.ReadAllBytes(filePath);
+                string fileString = Encoding.UTF8.GetString(fileBytes);
+                Console.WriteLine(filePath);
+                fileType = FileTypeFromPath(filePath);
+                Console.WriteLine(fileType);
+
+            }
+        }
+
+        private void save_iv_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.FileName = "test.txt";
+
+            DialogResult userClickedOK = saveFileDialog1.ShowDialog();
+            if (userClickedOK == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                string ivString = Encoding.UTF8.GetString(iv);
+
+                File.WriteAllText(filePath, ivString);
+                MessageBox.Show("File saved successfully.");
+            }
+        }
+
+        private void save_file_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.FileName = $"test.txt";
+
+            DialogResult userClickedOK = saveFileDialog1.ShowDialog();
+            if (userClickedOK == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                string fileBytesString = Encoding.UTF8.GetString(fileBytes);
+
+                File.WriteAllText(filePath, fileBytesString);
+                MessageBox.Show("File saved successfully.");
+            }
+        }
+
+        public static byte[] InicializaciskiVektor()
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] iv = new byte[16];
+                rng.GetBytes(iv);
+                Debug.WriteLine(Convert.ToBase64String(iv));
+                return iv;
+            }
+        }
+        public byte[] EncryptAes(byte[] data, byte[] key, byte[] iv, int keySize)
+        {
+            using (var aes = Aes.Create())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+                aes.KeySize = keySize;
+                aes.Padding = PaddingMode.Zeros;
+                aes.BlockSize = keySize;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using MemoryStream msEncrypt = new MemoryStream();
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    csEncrypt.Write(data, 0, data.Length);
+                    csEncrypt.FlushFinalBlock();
+                }
+                return msEncrypt.ToArray();
+            }
+        }
+        public string FileTypeFromPath(string filePath)
+        {
+            int extentionIndex = filePath.LastIndexOf('.');
+            if (extentionIndex != -1)
+            {
+                string result = filePath.Substring(extentionIndex);
+                return result;
+            }
+            else
+            {
+                return ".idk";
+            }
+        }
+
+        public byte[] DecryptAes(byte[] encryptedData, byte[] sharedSecret, byte[] userIV)
+        {
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = sharedSecret;
+                aesAlg.IV = userIV;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedData))
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                using (MemoryStream decryptedStream = new MemoryStream())
+                {
+                    csDecrypt.CopyTo(decryptedStream);
+
+                    return decryptedStream.ToArray();
+                }
+            }
+        }
+    }
+}
+
